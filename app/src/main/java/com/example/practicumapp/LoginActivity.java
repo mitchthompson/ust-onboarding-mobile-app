2fc570b57bc298bbbebf09ad4bd175471fc1ba65
Justin <just_255@yahoo.com>
2018-01-28 16:02:12 -0800

    new login

--- app/src/main/java/com/example/practicumapp/LoginActivity.java
+++ app/src/main/java/com/example/practicumapp/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.example.practicumapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.practicumapp.Interfaces.UserResponseCallback;
+import com.example.practicumapp.models.User;
 
 /**
  *
@@ -25,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText unameField, pswdField;
     Button submitLogin;
+    String userID, userPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +46,13 @@ public class LoginActivity extends AppCompatActivity {
         submitLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(unameField.getText().toString().equals("admin") && pswdField.getText().toString().equals("admin")){
+                userID = unameField.getText().toString();
+                userPass = pswdField.getText().toString();
+
+                UserLoginTask login = new UserLoginTask(userID, userPass);
+                login.doInBackground();
+
+                if(userID.equals("admin") && userPass.equals("admin")){
                     Toast.makeText(LoginActivity.this, "Username and Password is correct", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(LoginActivity.this, TaskListActivity.class));
                 }else{
@@ -49,5 +60,67 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                UserParser getJson = new UserParser(getApplicationContext());
+                getJson.getUser(mEmail, new UserResponseCallback() {
+                    @Override
+                    public void onSuccess(User user) {
+                        String temp = "Email: " + user.getEmail() + "\nFirst: " + user.getFirstName() + "\nLast: " + user.getLastName()
+                                + "\nUsername: " + user.getUserName() + "\nType: " + user.getType() + "\nID: " + user.getId()
+                                + "\nPhone: " + user.getPhone();
+                        Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_LONG).show();
+
+
+
+                    }
+                });
+
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                finish();
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
     }
 }
