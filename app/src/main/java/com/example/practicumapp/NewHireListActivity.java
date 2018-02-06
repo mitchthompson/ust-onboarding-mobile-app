2ded025892b730fbc74e3f4fac1bc7b61e415a5d
Mitch Thompson <thomitchell@gmail.com>
2018-02-06 15:01:53 -0800

    Adds volleyparser calls to the API for employees on new hire list

--- app/src/main/java/com/example/practicumapp/NewHireListActivity.java
+++ app/src/main/java/com/example/practicumapp/NewHireListActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -14,21 +15,26 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.SearchView;
 
+import com.example.practicumapp.Interfaces.VolleyUserResponseListener;
 import com.example.practicumapp.adapters.NewHireListAdapter;
+import com.example.practicumapp.models.User;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewHireListActivity extends AppCompatActivity {
+    private static final String TAG = NewHireListActivity.class.getName(); // Constant for logging data
     private Button addNewHireButton;
-    private SearchView searchView;
 
     private Context context;
-    private String[] testData;
     private RecyclerView recyclerView;
     private RelativeLayout relativeLayout;
     private NewHireListAdapter newHireListAdapter;
     private RecyclerView.LayoutManager recyclerViewLayoutManager;
+
+    private Map<String, String> employees;
     private ArrayList<String> newHireList;
 
     @Override
@@ -53,19 +59,44 @@ public class NewHireListActivity extends AppCompatActivity {
             }
         });
 
+        employees = new HashMap<>();
+        newHireList = new ArrayList<>();
+        VolleyParser volleyParser = new VolleyParser(this.getApplicationContext());
+        volleyParser.getUser("72AD9DBC60AE485782D43A1AE09279A4", new VolleyUserResponseListener() {
+            @Override
+            public void onSuccess(User user) {
+                Log.d(TAG, "New Hire Employees : " + user.getEmployees().toString());
+                employees = user.getEmployees();
+
+                for(Map.Entry<String, String> entry : employees.entrySet()){
+                    Log.d(TAG, "Key: " + entry.getKey() + "Value: " + entry.getValue());
+                    newHireList.add(entry.getValue());
+                }
+
+                relativeLayout = (RelativeLayout) findViewById(R.id.new_hire_relativeLayout);
+                recyclerView = (RecyclerView) findViewById(R.id.new_hire_list_recycler);
+                recyclerViewLayoutManager = new LinearLayoutManager(context);
+                recyclerView.setLayoutManager(recyclerViewLayoutManager);
+
+                newHireListAdapter = new NewHireListAdapter(context, newHireList);
+                recyclerView.setAdapter(newHireListAdapter);
+
+            }
+        });
+
         /**
          * TODO: get data from JSON parser, remove test dada
          */
-        newHireList = new ArrayList<>(Arrays.asList("John Doe", "Susan Smith", "Blake Bortles",
-                "Michael Barnum", "Jason Mendoza", "Jill Riker", "Andrew Ryan", "Frank Fontaine", "Generic Name", "Reference Name"));
+//        newHireList = new ArrayList<>(Arrays.asList("John Doe", "Susan Smith", "Blake Bortles",
+//                "Michael Barnum", "Jason Mendoza", "Jill Riker", "Andrew Ryan", "Frank Fontaine", "Generic Name", "Reference Name"));
 
-        relativeLayout = (RelativeLayout) findViewById(R.id.new_hire_relativeLayout);
-        recyclerView = (RecyclerView) findViewById(R.id.new_hire_list_recycler);
-        recyclerViewLayoutManager = new LinearLayoutManager(context);
-        recyclerView.setLayoutManager(recyclerViewLayoutManager);
-
-        newHireListAdapter = new NewHireListAdapter(context, newHireList);
-        recyclerView.setAdapter(newHireListAdapter);
+//        relativeLayout = (RelativeLayout) findViewById(R.id.new_hire_relativeLayout);
+//        recyclerView = (RecyclerView) findViewById(R.id.new_hire_list_recycler);
+//        recyclerViewLayoutManager = new LinearLayoutManager(context);
+//        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+//
+//        newHireListAdapter = new NewHireListAdapter(context, newHireList);
+//        recyclerView.setAdapter(newHireListAdapter);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
