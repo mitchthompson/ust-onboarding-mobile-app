70dd8e769fe7946682f31e9392cdb5a4625f6b02
Joseph Sayler <joseph.sayler@outlook.com>
2018-02-10 04:56:56 +0000

    Revert "added ADAL login code to MainActivity
    copied the code added to LoginActivity and put it in onCreate of
    MainActivity. added buttons to the various activities, removed button
    for LoginActivity, edited the menu items: added code to start
    AddNewHireActivity, commented out code to start LoginActivity.
    Added/edited toast for Logout and Login, respectively. finally, removed
    LoginActivity_AS_Template.java and activity_login_as_template.xml"

--- app/src/main/java/com/example/practicumapp/MainActivity.java
+++ app/src/main/java/com/example/practicumapp/MainActivity.java
@@ -1,38 +1,28 @@
 package com.example.practicumapp;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.practicumapp.Interfaces.VolleyUserResponseListener;
 import com.example.practicumapp.Interfaces.VolleyWorkflowResponseListener;
-import com.example.practicumapp.helpers.Constants;
 import com.example.practicumapp.models.User;
 import com.example.practicumapp.models.Workflow;
-import com.microsoft.aad.adal.AuthenticationCallback;
-import com.microsoft.aad.adal.AuthenticationContext;
-import com.microsoft.aad.adal.AuthenticationException;
-import com.microsoft.aad.adal.AuthenticationResult;
-import com.microsoft.aad.adal.PromptBehavior;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Button taskListButton;
-    //private Button loginButton;
-    private Button newHireListButton;
-    private Button addNewHireButton;
+    private Button loginButton;
     private static final String TAG = MainActivity.class.getName(); // Constant for logging data
-    private AuthenticationCallback<AuthenticationResult> callback;
-    private AuthenticationContext mContext;
-    private static final String TAG1 = "ADAL MAIN ACT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,34 +36,22 @@ public class MainActivity extends AppCompatActivity {
         myToolbar.setTitle("Main Activity");
         setSupportActionBar(myToolbar);
 
+
         // TODO: remove temporary test buttons when not needed
 
-        taskListButton = findViewById(R.id.task_list_button);
-        //loginButton = findViewById(R.id.login_button);
-        newHireListButton = findViewById(R.id.newhire_list_button);
-        addNewHireButton = findViewById(R.id.newhire_add_button);
 
-        /**loginButton.setOnClickListener(new View.OnClickListener() {
+        taskListButton = findViewById(R.id.task_list_button);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
-        });**/
+        });
 
         taskListButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, TaskListActivity.class));
-            }
-        });
-
-        newHireListButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, NewHireListActivity.class));
-            }
-        });
-
-        addNewHireButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, AddNewHireActivity.class));
             }
         });
 
@@ -91,53 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "VolleyParser Workflow Task Name : " + workflow.getTasks().get(0).getName());
             }
         });
-
-        // ADAL AAD login code
-//------------------------------------------------------------------------------------------------\\
-        // creates new AuthenticationContext object
-        mContext = new AuthenticationContext(MainActivity.this, Constants.AUTHORITY_URL,
-                true);
-        // callback used when asking for a token
-        callback = new AuthenticationCallback<AuthenticationResult>() {
-
-            @Override
-            public void onError(Exception exc) {
-                if (exc instanceof AuthenticationException) {
-                    //textViewStatus.setText("Cancelled");
-                    Log.d(TAG1, "Cancelled");
-                } else {
-                    //textViewStatus.setText("Authentication error:" + exc.getMessage());
-                    Log.d(TAG1, "Authentication error:" + exc.getMessage());
-                }
-            }
-
-            @Override
-            public void onSuccess(AuthenticationResult result) {
-                //mResult = result;
-
-                if (result == null || result.getAccessToken() == null || result.getAccessToken().isEmpty()) {
-                    //textViewStatus.setText("Token is empty");
-                    Log.d(TAG1, "Token is empty");
-                } else {
-                    // request is successful
-                    Log.d(TAG1, "Status:" + result.getStatus() + " Expires:" + result.getExpiresOn().toString());
-                    //textViewStatus.setText(PASSED);
-                }
-            }
-        };
-        // asks for a token by using the callback
-        mContext.acquireToken(MainActivity.this, Constants.RESOURCE_ID, Constants.CLIENT_ID,
-                Constants.REDIRECT_URL, Constants.USER_HINT, PromptBehavior.Auto, "", callback);
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (mContext != null) {
-            mContext.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-//------------------------------------------------------------------------------------------------\\
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -151,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_logout:
                 // User chose the "Log Out" item...
-                Toast.makeText(getApplicationContext(), "This does nothing at the moment",
+                Toast.makeText(getApplicationContext(), "Logout toast. Cheers!",
                         Toast.LENGTH_SHORT).show();
 
                 return true;
@@ -159,9 +91,7 @@ public class MainActivity extends AppCompatActivity {
             // TODO Remove following action items from actionbar here and in res/main_menu.xml
 
             case R.id.action_login:
-                //startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                Toast.makeText(getApplicationContext(), "This does nothing at the moment",
-                        Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
                 return true;
 
             case R.id.action_tasklist:
@@ -173,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
 
             case R.id.action_addhire:
-                startActivity(new Intent(MainActivity.this, AddNewHireActivity.class));
+                //startActivity(new Intent(MainActivity.this, ActivityName.class));
                 return true;
 
             default:
