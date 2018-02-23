a61b394b09caffc1e723f6723a90ff8db591b9fb
Mitch Thompson <thomitchell@gmail.com>
2018-02-23 13:38:35 -0800

    Adds a logic to build hashmap of new user from AddNewHireActivity form on user submission. Comments out api call to add new user since still need to get list of all workflows. Tweaks UI of activity a bit. Adds form validation for email, phone, and workflow.

--- app/src/main/java/com/example/practicumapp/AddNewHireActivity.java
+++ app/src/main/java/com/example/practicumapp/AddNewHireActivity.java
@@ -4,9 +4,14 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+
 import android.support.v7.widget.Toolbar;
+import android.telephony.PhoneNumberUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,27 +20,29 @@ import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.DatePicker;
-import android.app.DatePickerDialog;
+
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.text.TextUtils;
-import android.app.ProgressDialog;
-import android.widget.ProgressBar;
 
-import org.w3c.dom.Text;
+import com.example.practicumapp.Interfaces.VolleyUserResponseListener;
+import com.example.practicumapp.models.User;
 
 public class AddNewHireActivity extends AppCompatActivity {
+    private static final String TAG = AddNewHireActivity.class.getName(); // Constant for logging data
 
-    private Spinner spinner1;
+    private Spinner workflow;
     private Button btnCancel, btnDone;
     private DatePickerDialog.OnDateSetListener onDateSetListener;
     private EditText firstName, lastName, email, phone, date;
+    private Map<String,String> newUser;
 
 
     @Override
@@ -52,12 +59,12 @@ public class AddNewHireActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
 
         // Views
-        firstName = (EditText) findViewById(R.id.first_name);
-        lastName = (EditText) findViewById(R.id.last_name);
-        email = (EditText) findViewById(R.id.email);
-        phone = (EditText) findViewById(R.id.phone);
-        date = (EditText) findViewById(R.id.date);
-
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+        date = findViewById(R.id.date);
+        addItemsOnWorkflowSpinner();
 
 
         // Add calendar settings for date picker
@@ -99,6 +106,142 @@ public class AddNewHireActivity extends AppCompatActivity {
         };
     }
 
+    /**
+     * Adds listeners for drop down menu selection for workflows
+     */
+    public void addListenerOnSpinnerItemSelection() {
+        workflow = findViewById(R.id.workflow_ID);
+    }
+
+    /**
+     * Add listeners for spinner and buttons
+     */
+    public void addListenerOnButton() {
+
+        // Add listeners to all page buttons
+        workflow = findViewById(R.id.workflow_ID);
+        btnDone = findViewById(R.id.btnDone);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        // Perform activity when "Done" is selected -- for now just displays toast confirming drop
+        // down selection
+        btnDone.setOnClickListener(new OnClickListener() {
+            //add toast to display selection
+            @Override
+            public void onClick(View v) {
+                addEmployee();
+            }
+        });
+
+        btnCancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddNewHireActivity.this, NewHireListActivity.class));
+            }
+        });
+    }
+
+    // add items into spinner dynamically
+    public void addItemsOnWorkflowSpinner() {
+        //TODO: get workflow list from api call
+        workflow = findViewById(R.id.workflow_ID);
+        List<String> list = new ArrayList<>();
+        list.add("Select Workflow");
+        list.add("Workflow 1");
+        list.add("Workflow 2");
+        list.add("Workflow 3");
+        list.add("Workflow 4");
+        list.add("Workflow 5");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        workflow.setAdapter(dataAdapter);
+    }
+
+    /**
+     * Will be called when user clicks "Done" button on form. Validates form inputs and adds employee via
+     * the API. Errors generate a message and stop the execution.
+     */
+    //TODO: Call this method when "Done" button is clicked
+    public void addEmployee(){
+        String inputFirstName = firstName.getText().toString().trim();
+        String inputLastName = lastName.getText().toString().trim();
+        String inputEmail = email.getText().toString().trim();
+        String inputPhone = phone.getText().toString().trim();
+        //TODO: see if date picker EditText view can be cast to string in this way
+        String inputDate = date.getText().toString().trim();
+        String inputWorkflow = String.valueOf(workflow.getSelectedItem());
+
+        // Form validation for all fields
+        if(TextUtils.isEmpty(inputFirstName)){
+            // Show message
+            Toast.makeText(this, "Please enter a first name", Toast.LENGTH_SHORT).show();
+
+            // Stops function from executing
+            return;
+        }
+
+        if(TextUtils.isEmpty(inputLastName)){
+            // Show message
+            Toast.makeText(this, "Please enter a last name", Toast.LENGTH_SHORT).show();
+
+            // Stops function from executing
+            return;
+        }
+
+        if(TextUtils.isEmpty(inputEmail) || !Patterns.EMAIL_ADDRESS.matcher(inputEmail).matches()){
+            // Show message
+            Toast.makeText(this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
+
+            // Stops function from executing
+            return;
+        }
+
+        if(TextUtils.isEmpty(inputPhone) || !Patterns.PHONE.matcher(inputPhone).matches() ){
+            // Show message
+            Toast.makeText(this, "Please enter a valid phone number", Toast.LENGTH_SHORT).show();
+
+            // Stops function from executing
+            return;
+        }
+
+        if(TextUtils.isEmpty(inputDate)){
+            // Show message
+            Toast.makeText(this, "Please enter the hire date", Toast.LENGTH_SHORT).show();
+
+            // Stops function from executing
+            return;
+        }
+
+        if(String.valueOf(workflow.getSelectedItem()) == "Select Workflow"){
+            // Show message
+            Toast.makeText(this, "Please select a workflow", Toast.LENGTH_SHORT).show();
+
+            // Stops function from executing
+            return;
+        }
+
+        newUser = new HashMap<>();
+        newUser.put("firstName", inputFirstName);
+        newUser.put("lastName", inputLastName);
+        newUser.put("email", inputEmail);
+        newUser.put("phone", inputPhone);
+        newUser.put("startDate", inputDate);
+        newUser.put("workflow", inputWorkflow);
+
+        Log.d(TAG, "User: " + newUser.toString());
+
+//        VolleyParser volleyParser = new VolleyParser(this.getApplicationContext());
+//        volleyParser.addNewUser((User) newUser,  new VolleyUserResponseListener(){
+//                    @Override
+//                    public void onSuccess(User user) {
+//                        startActivity(new Intent(AddNewHireActivity.this, NewHireListActivity.class));
+//                    }
+//        });
+
+        }
+
     // Creates menu
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -124,102 +267,5 @@ public class AddNewHireActivity extends AppCompatActivity {
 
         }
     }
-
-    /**
-     * Adds listeners for drop down menu selection for workflows
-     */
-    public void addListenerOnSpinnerItemSelection() {
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
-    }
-
-    /**
-     * Add listeners for spinner and buttons
-     */
-    public void addListenerOnButton() {
-
-        // Add listeners to all page buttons
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
-        btnDone = (Button) findViewById(R.id.btnDone);
-        btnCancel = (Button) findViewById(R.id.btnCancel);
-
-
-        // Perform activity when "Done" is selected -- for now just displays toast confirming drop
-        // down selection
-        btnDone.setOnClickListener(new OnClickListener() {
-
-            //add toast to display selection
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(AddNewHireActivity.this,
-                        "OnClickListener : " +
-                                "\nSpinner : "+ String.valueOf(spinner1.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
-            }
-
-        });
-    }
-
-    /**
-     * Will be called when user clicks "Done" button on form. Validates form inputs and adds employee via
-     * the API. Errors generate a message and stop the execution.
-     */
-    //TODO: Call this method when "Done" button is clicked
-    public void addEmployee(){
-        String inputFirstName = firstName.getText().toString().trim();
-        String inputLastName = lastName.getText().toString().trim();
-        String inputEmail = email.getText().toString().trim();
-        String inputPhone = phone.getText().toString().trim();
-        //TODO: see if date picker EditText view can be cast to string in this way
-        String inputDate = date.getText().toString().trim();
-        //TODO: add variable for workflow once API workflow is determined (ie. number to indicate which workflow)
-
-        // Form validation for all fields
-        if(TextUtils.isEmpty(inputFirstName)){
-            // Show message
-            Toast.makeText(this, "Please enter first name", Toast.LENGTH_SHORT).show();
-
-            // Stops function from executing
-            return;
-        }
-
-        if(TextUtils.isEmpty(inputLastName)){
-            // Show message
-            Toast.makeText(this, "Please enter last name", Toast.LENGTH_SHORT).show();
-
-            // Stops function from executing
-            return;
-        }
-
-        if(TextUtils.isEmpty(inputEmail)){
-            // Show message
-            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
-
-            // Stops function from executing
-            return;
-        }
-
-        if(TextUtils.isEmpty(inputPhone)){
-            // Show message
-            Toast.makeText(this, "Please enter phone", Toast.LENGTH_SHORT).show();
-
-            // Stops function from executing
-            return;
-        }
-
-        if(TextUtils.isEmpty(inputDate)){
-            // Show message
-            Toast.makeText(this, "Please enter hire date", Toast.LENGTH_SHORT).show();
-
-            // Stops function from executing
-            return;
-        }
-
-        // If validations are OK, show progress bar
-        //TODO: Add progress bar
-
-        //TODO: Add API POST method to add employee
-
-        }
 
     }
