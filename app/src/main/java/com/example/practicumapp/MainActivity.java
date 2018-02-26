33cee761558a6917c409d424fc1548e806c81e1d
Joseph Sayler <joseph.sayler@gmail.com>
2018-02-25 20:37:46 -0800

    Added javadoc documentation

--- app/src/main/java/com/example/practicumapp/MainActivity.java
+++ app/src/main/java/com/example/practicumapp/MainActivity.java
@@ -21,6 +21,12 @@ import com.microsoft.aad.adal.AuthenticationException;
 import com.microsoft.aad.adal.AuthenticationResult;
 import com.microsoft.aad.adal.PromptBehavior;
 
+/**
+ * Basic login activity using ADAL. Displays a sign in page to enter credentials and gain access to system resources.
+ * This version runs from MainActivity instead of its own LoginActivity.
+ * @author Joseph Sayler
+ * @version 1.2
+ **/
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +38,17 @@ public class MainActivity extends AppCompatActivity {
     private final static String EMPLOYEE = "employee@tkarp87live.onmicrosoft.com";
     private final static String MANAGER = "manager@tkarp87live.onmicrosoft.com";
 
+    /**
+     * AuthContext and AuthCalback are created upon activity creation.
+     *
+     * AuthenticationContext is required to set up the 'context' of the authentication - it
+     * provides URL and other necessary info to start the login process.
+     * AuthenticationCallback is required to get a token from AAD.
+     * AuthenticationContext uses callback to request the token. The act of requesting the token
+     * opens a WebView that loads the AAD login screen. The results of authentication (if successful)
+     * are stored in AuthenticationResult and provide information such as token ID, username, etc.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         login_button = findViewById(R.id.loginButton);
+        // initiates login on button press
         login_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d(TAG,"Requesting login token");
@@ -74,12 +92,33 @@ public class MainActivity extends AppCompatActivity {
         });
      }
 
+    /**
+     * Handles the end of AuthenticationActivity after user enters credentials and receives authorization code.
+     * @param requestCode indicates the type of request
+     * @param resultCode indicates result of the login attempt
+     * @param data data returned after request has been made
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (mContext != null) {
             mContext.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    /**
+     * Removes user token and cleans up cache. Acts as a way to log out of AD. Takes user back to
+     * main activity screen once called.
+     */
+    protected void adalLogout() {
+        CookieSyncManager.createInstance(getApplicationContext());
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeSessionCookie();
+        CookieSyncManager.getInstance().sync();
+        mContext.getCache().removeAll();
+        mResult = null;
+        startActivity(new Intent(MainActivity.this, MainActivity.class));
+        Log.d(TAG,"User Logout");
     }
 
     /**
@@ -94,6 +133,11 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Assigns functions to menu items
+     * @param item the inflated menu where the functions are associated
+     * @return returns true unless the default case is reached, then it returns super.onOptionsItemSelected(item)
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -102,14 +146,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG,"debug menu activity selected");
                 break;
             case R.id.action_logout:
-                CookieSyncManager.createInstance(getApplicationContext());
-                CookieManager cookieManager = CookieManager.getInstance();
-                cookieManager.removeSessionCookie();
-                CookieSyncManager.getInstance().sync();
-                mContext.getCache().removeAll();
-                mResult = null;
-                startActivity(new Intent(MainActivity.this, MainActivity.class));
-                Log.d(TAG,"User Logout");
+                adalLogout();
                 break;
             default:
                 // If we got here, the user's action was not recognized.
@@ -119,6 +156,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * TEST CODE - based on one of 2 emails used, routes user to one of 2 activities
+     */
     private void loginRouter() {
         if(mResult.getUserInfo().getDisplayableId().equals(EMPLOYEE)) {
             Log.d(TAG, "employee login - move to TaskListActivity");
@@ -132,6 +172,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * DISPLAYS DEBUG DATA IN LOGCAT
+     */
     private void tokenLogData() {
         Log.d(TAG, "Status: " + mResult.getStatus().toString() +
                 "\nLogin expires: " + mResult.getExpiresOn().toString() +
