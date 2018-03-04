606be4a5c077fec3e8be75091c1eac86903aaec9
supreti17 <upretisuraj11@gmail.com>
2018-03-04 13:18:32 -0800

    - Minor update

--- app/src/main/java/com/example/practicumapp/Interfaces/VolleyListResponseListener.java
+++ app/src/main/java/com/example/practicumapp/Interfaces/VolleyListResponseListener.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
  * This interface enable callbacks to any activity after workflows list response is received using volley
  */
 
-public interface VolleyWorkflowsListResponseListener {
+public interface VolleyListResponseListener {
 
     void onSuccess(HashMap<String, String> workflows); // Called after workflows list json response is received successfully
 
